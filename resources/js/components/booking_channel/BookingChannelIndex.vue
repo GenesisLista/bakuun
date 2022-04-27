0275f0ba841aa1b8358da175a12508f56cdd7631
Genesis Lista <genesis_lista@sayeghwater.com>
2022-04-27 10:07:46 +0300

    Update app css

--- resources/js/components/booking_channel/BookingChannelIndex.vue
+++ resources/js/components/booking_channel/BookingChannelIndex.vue
@@ -8,28 +8,27 @@
 <div class="overflow-hidden overflow-x-auto min-w-full align-middle sm:rounded-md">
 	<table class="min-w-full border divide-y divide-gray-200">
 		<thead>
-		<tr>
-			<th class="px-6 py-3 bg-gray-50">
-			<span class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Name</span>
-			</th>
-			<th class="px-6 py-3 bg-gray-50">
-			<span class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Action</span>
-			</th>
-		</tr>
+			<tr>
+				<th class="px-6 py-3 bg-gray-50">
+					<span class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Name</span>
+				</th>
+				<th class="px-6 py-3 bg-gray-50">
+					<span class="text-xs font-medium tracking-wider leading-4 text-left text-gray-500 uppercase">Action</span>
+				</th>
+			</tr>
 		</thead>
 
 		<tbody class="bg-white divide-y divide-gray-200 divide-solid">
 			<template v-for="bchannel in bookingChannels" :key="bchannel.id">
-			<tr class="bg-white">
-				<td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
-				{{ bchannel.name }}
-				</td>
-				<td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
-				<router-link :to="{ name: 'bookingchannel.edit', params: { id: bchannel.id } }" class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Edit</router-link>
-				
-				<button @click="destroyBookingChannel(bchannel.id)"  class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Delete</button>
-				</td>
-			</tr>
+				<tr class="bg-white">
+					<td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+						{{ bchannel.name }}
+					</td>
+					<td class="px-6 py-4 text-sm leading-5 text-gray-900 whitespace-no-wrap">
+						<router-link :to="{ name: 'bookingchannel.edit', params: { id: bchannel.id } }" class="mr-2 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Edit</router-link>
+						<button @click="destroyBookingChannel(bchannel.id)"  class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Delete</button>
+					</td>
+				</tr>
 			</template>
 		</tbody>
 	</table>
